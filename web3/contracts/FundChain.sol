98c2af7be86995ab1ba90d883adc43ab771814ba
manishjadhav9 <manishsj289@gmail.com>
2025-01-26 18:00:22 +0530

    createCampaign functions contract completed

--- web3/contracts/FundChain.sol
+++ web3/contracts/FundChain.sol
@@ -7,7 +7,7 @@ contract FundChain {
         string title;
         string description;
         uint256 target;
-        uint256 deadlin;
+        uint256 deadline;
         uint256 amountCollected;
         string image;
         address[] donators;
@@ -19,7 +19,24 @@ contract FundChain {
 
     uint256 public numberOfCampaigns = 0;
 
-    function createCampaign() {}
+    function createCampaign(address _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _image) public returns (uint256) {
+        Campaign storage campaign = campaigns[numberOfCampaigns];
+
+        // is everything okay?
+        require(campaign.deadline < block.timestamp, "The deadline should be a date in the future.");
+
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.amountCollected = 0;
+        campaign.image = _image;
+
+        numberOfCampaigns++;
+
+        return numberOfCampaigns - 1;
+    }
 
     function donateToCampaign() {}
 
